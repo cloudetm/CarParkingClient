942c86f5378c1b99c7eff61f448324c95ffedeb9
gnanasuriyan <msgtosuriya@gmail.com>
2014-11-29 13:52:29 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* App Module */
+
+var phonecatApp  = angular.module('phonecatApp', [
+	'ngRoute',
+	'phonecatControllers'
+]);
+
+phonecatApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/phones', {
+		templateUrl : 'partials/phone-list.html',
+		controller: 'PhoneListCtrl'
+	}).when('/phones/:phoneId', {
+		templateUrl : 'partials/phone-details.html',
+		controller: 'PhoneDetailsCtrl'
+	}).otherwise({
+		redirectTo: '/phones'
+	});
+}]);
